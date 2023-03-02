aa67712febd90cd41b1c6273e555e070c4c0b541
DawidDabrowskii <dabrowskidawid680@gmail.com>
2023-03-02 15:24:52 +0100

    Initial

--- README.md
+++
@@ -1,2 +0,0 @@
-# Website
-My portfolio website
---
+++ portfolio/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const NavBar = () => {
+  return (
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+      <h1>Dawid Dąbrowski</h1>
+    </div>
+  );
+};
+
+export default NavBar;
