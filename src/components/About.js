98362429e3d8e4112dc65356ff230ba31748c575
DawidDabrowskii <dabrowskidawid680@gmail.com>
2023-03-05 13:37:27 +0100

    Update
    -Adding icons and their links,
    -Adding 'About' section,
    -Minor style changes

---
+++ src/components/About.js
@@ -0,0 +1,37 @@
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-20">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo quasi
+          ducimus tempora laboriosam id modi velit dicta, quibusdam, sed
+          voluptatibus ea commodi doloremque. Officiis inventore ducimus, neque
+          cumque, itaque dolorem saepe rem distinctio recusandae ipsa a quisquam
+          quis aut sit ea error veritatis pariatur illum ab ratione quam enim
+          explicabo.
+        </p>
+        <br />
+
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus
+          consequatur obcaecati atque sunt et culpa dignissimos laudantium
+          voluptatibus voluptas, iure repellendus velit architecto ab tempore
+          necessitatibus vitae tempora incidunt inventore praesentium
+          accusantium! Error alias aliquid fuga pariatur deserunt deleniti
+          reprehenderit architecto dolorem. Eveniet maiores minima omnis non
+          eligendi animi consectetur!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
       className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row md:gap-5">
-        <div className="flex flex-col justify-center h-full ">
+        <div className="flex flex-col justify-center h-full lg:ml-14 xl:ml-0">
           <h2 className="text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-bold text-white">
             I'm a Frontend Developer
           </h2>
