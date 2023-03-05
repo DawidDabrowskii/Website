a329432a74933280584a95fe65ccad34fdab8000
DawidDabrowskii <dabrowskidawid680@gmail.com>
2023-03-05 12:53:40 +0100

    Removing one unnecessary folder

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row md:gap-5">
         <div className="flex flex-col justify-center h-full ">
-          <h2 className="text-4xl sm:text-5xl md:text-7xl font-bold text-white">
+          <h2 className="text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-bold text-white">
             I'm a Frontend Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
